6a8365cb465fbf5dcccc84142f6cac9ecbb40448
sameerrM <samir_nox@hotmail.com>
2021-12-13 15:25:13 +0100

    typer speed

--- src/components/Typer/Typer.js
+++ src/components/Typer/Typer.js
@@ -1,48 +1,51 @@
-import React from 'react';
+import React from "react"
 
 class Typer extends React.Component {
   state = {
-    text: '',
+    text: "",
     isDeleting: false,
     loopNum: 0,
-    typingSpeed: 150
+    typingSpeed: 150,
   }
 
   componentDidMount() {
-    this.handleType();
+    this.handleType()
   }
 
   handleType = () => {
-    const { dataText } = this.props;
-    const { isDeleting, loopNum, text, typingSpeed } = this.state;
-    const i = loopNum % dataText.length;
-    const fullText = dataText[i];
+    const { dataText } = this.props
+    const { isDeleting, loopNum, text, typingSpeed } = this.state
+    const i = loopNum % dataText.length
+    const fullText = dataText[i]
 
     this.setState({
-      text: isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1),
-      typingSpeed: isDeleting ? 30: 150
-    });
+      text: isDeleting
+        ? fullText.substring(0, text.length - 1)
+        : fullText.substring(0, text.length + 1),
+      typingSpeed: isDeleting ? 30 : 80,
+    })
 
     if (!isDeleting && text === fullText) {
-      setTimeout(() => this.setState({ isDeleting: true }), 500);
-    } else if (isDeleting && text === '') {
+      setTimeout(() => this.setState({ isDeleting: true }), 500)
+    } else if (isDeleting && text === "") {
       this.setState({
         isDeleting: false,
-        loopNum: loopNum + 1
-      });
+        loopNum: loopNum + 1,
+      })
     }
 
-    setTimeout(this.handleType, typingSpeed);
-  };
+    setTimeout(this.handleType, typingSpeed)
+  }
 
   render() {
     return (
-      <h4>{ this.props.heading }&nbsp;
-        <span>{ this.state.text }</span>
+      <h4>
+        {this.props.heading}&nbsp;
+        <span>{this.state.text}</span>
         <span id="cursor"></span>
       </h4>
-    );
+    )
   }
 }
 
-export default Typer;
+export default Typer
