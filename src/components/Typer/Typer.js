f720665ef4db7ca8d6b7a18f98dd119bed135d4f
Samir <42714951+sameerrM@users.noreply.github.com>
2022-06-12 23:49:16 +0200

    Update Typer.js

--- src/components/Typer/Typer.js
+++ src/components/Typer/Typer.js
@@ -22,7 +22,7 @@ class Typer extends React.Component {
       text: isDeleting
         ? fullText.substring(0, text.length - 1)
         : fullText.substring(0, text.length + 1),
-      typingSpeed: isDeleting ? 30 : 150,
+      typingSpeed: isDeleting ? 30 : 130,
     })
 
     if (!isDeleting && text === fullText) {
